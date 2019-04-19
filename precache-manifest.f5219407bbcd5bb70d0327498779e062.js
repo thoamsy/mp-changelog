a4cf15a85497c7249e0d0388832c788fb3cd61de
Yang Kui <c6swift@gmail.com>
2019-04-19 19:25:25 +0800

    Updates

---
+++ precache-manifest.f5219407bbcd5bb70d0327498779e062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b21a79ed1cbd8daa7e86",
+    "url": "/static/css/main.857eb92e.chunk.css"
+  },
+  {
+    "revision": "b21a79ed1cbd8daa7e86",
+    "url": "/static/js/main.5eece939.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "922f062145e6e52c5cfb",
+    "url": "/static/css/2.8c96bff4.chunk.css"
+  },
+  {
+    "revision": "922f062145e6e52c5cfb",
+    "url": "/static/js/2.111f151b.chunk.js"
+  },
+  {
+    "revision": "debfdd354416d8d9f2205370bad5debf",
+    "url": "/index.html"
+  }
+];
