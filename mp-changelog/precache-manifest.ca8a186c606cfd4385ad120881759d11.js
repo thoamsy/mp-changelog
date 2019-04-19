a78a98041b06dc48f156ee57244bc95ab41ea172
Yang Kui <c6swift@gmail.com>
2019-04-19 19:42:55 +0800

    Updates

---
+++ mp-changelog/precache-manifest.ca8a186c606cfd4385ad120881759d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b41e2614c78e74c065d5",
+    "url": "/mp-changelog/static/css/main.857eb92e.chunk.css"
+  },
+  {
+    "revision": "b41e2614c78e74c065d5",
+    "url": "/mp-changelog/static/js/main.97fea47e.chunk.js"
+  },
+  {
+    "revision": "8ee3ab07fa705dad98eb",
+    "url": "/mp-changelog/static/js/runtime~main.d1fec894.js"
+  },
+  {
+    "revision": "33c23a95026716161e18",
+    "url": "/mp-changelog/static/css/2.8c96bff4.chunk.css"
+  },
+  {
+    "revision": "33c23a95026716161e18",
+    "url": "/mp-changelog/static/js/2.8c639fb5.chunk.js"
+  },
+  {
+    "revision": "36c0902dc81c824476387b6ca8e5dc65",
+    "url": "/mp-changelog/index.html"
+  }
+];
