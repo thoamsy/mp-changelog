887349ffb40e525b7fa7dcbae1dd7c196843bab6
Yang Kui <c6swift@gmail.com>
2019-04-19 11:26:23 +0800

    fix: 添加 title

--- src/ChangeLogDrawer.jsx
+++ src/ChangeLogDrawer.jsx
@@ -6,13 +6,15 @@ import Markdown from 'markdown-it';
 
 const md = new Markdown();
 
-const ChangeLogDrawer = ({ changelog, visible, toggleDetail, onClose }) => {
+const ChangeLogDrawer = ({ changelog, visible, onClose }) => {
   const changelogWithParse = useMemo(() => md.render(changelog), [changelog]);
   return (
     <Drawer
       width={500}
+      title="更新日志"
       placement="right"
       maskClosable
+      closable={false}
       onClose={onClose}
       visible={visible}
     >
